e06929da7328809dbf91cd8f4cd85115d0803939
marek-kaminski <Taraj720ab>
2021-11-16 19:51:15 +0100

    Dodaj pliki projektów.

---
+++ RPGcharacters/RPGcharacters/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using RPGcharacters.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace RPGcharacters.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
